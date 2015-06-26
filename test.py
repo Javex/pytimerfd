ffc8a9a4b1a25a0089de759487838dca19448f0f
javex <florian.ruechel@inexplicity.de>
2015-06-27 01:37:38 +0200

    Improve tests

--- test.py
+++ test.py
@@ -3,10 +3,14 @@ import select
 import os
 
 timer = timerfd.timerfd(timerfd.CLOCK_MONOTONIC, timerfd.TFD_NONBLOCK)
-timer.settime(0, 0.5)
-timer.settime(0, 0.5)
+timer.settime(0, value=0.5, interval=0.5)
+print(timer.gettime())
+timer.settime(0, value=0.5, interval=0.5)
+print(timer.gettime())
 
 epoll = select.epoll()
 epoll.register(timer.fileno(), select.EPOLLIN)
-for fd, mask in epoll.poll(-1):
-    print(timerfd.unpack(os.read(fd, 8)))
+while True:
+    for fd, mask in epoll.poll(-1):
+        print(timer.gettime())
+        print(timerfd.unpack(os.read(fd, 8)))
